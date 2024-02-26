0c15b700f794b80f34ffeeef3e5c05f683fc87d1
Kabir4874 <kabir.cse.bd@gmail.com>
2024-02-26 17:39:48 +0600

    add

--- tour-plan-app/src/App.jsx
+++ tour-plan-app/src/App.jsx
@@ -8,6 +8,14 @@ function App() {
     const newTours = tours.filter((tour) => tour.id !== id);
     setTours(newTours);
   }
+  if (tours.length === 0) {
+    return (
+      <div className="refresh">
+        <h2>No Tours Left</h2>
+        <button onClick={() => setTours(data)}>Refresh</button>
+      </div>
+    );
+  }
   return (
     <div>
       <Tours tours={tours} removeTour={removeTour}></Tours>
