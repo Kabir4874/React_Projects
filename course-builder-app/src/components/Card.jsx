1da071a076145f283320f41281be09d3c685ce3e
Kabir4874 <kabir.cse.bd@gmail.com>
2024-02-28 18:57:16 +0600

    add

--- course-builder-app/src/components/Card.jsx
+++ course-builder-app/src/components/Card.jsx
@@ -1,17 +1,26 @@
 import { FcLike } from "react-icons/fc";
 import PropTypes from "prop-types";
+import { toast } from "react-toastify";
 
 const Card = ({ course, likedCourses, setLikedCourses }) => {
   function clickHandler() {
     if (likedCourses.includes(course.id)) {
-      setLikedCourses((prev) => prev.filter(cid !== course.id));
+      setLikedCourses((prev) => prev.filter((cid) => cid !== course.id));
+      toast.warning("Like Removed");
+    } else {
+      if (likedCourses.length === 0) {
+        setLikedCourses([course.id]);
+      } else {
+        setLikedCourses((prev) => [...prev, course.id]);
+      }
+      toast.success("Liked Successfully");
     }
   }
   return (
     <div className=" w-[300px] bg-bgDark bg-opacity-80 rounded-md overflow-hidden">
       <div className=" relative">
         <img src={course.image.url} />
-        <div className=" w-[40px] h-[40px] bg-white rounded-full absolute right-2 bottom-3 grid place-items-center">
+        <div className=" w-[40px] h-[40px] bg-white rounded-full absolute right-2 -bottom-4 grid place-items-center">
           <button onClick={clickHandler}>
             <FcLike fontSize="1.75rem" />
           </button>
