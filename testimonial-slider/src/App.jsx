77cc5a144518ffebddd7ac6200974019332a791b
Kabir4874 <kabir.cse.bd@gmail.com>
2024-02-28 20:49:42 +0600

    add

---
+++ testimonial-slider/src/App.jsx
@@ -0,0 +1,17 @@
+import Testimonial from "./components/Testimonial"
+
+function App() {
+
+
+  return (
+    <div>
+      <div>
+        <h1>Our Testimonials</h1>
+        <div></div>
+        <Testimonial/>
+      </div>
+    </div>
+  )
+}
+
+export default App
