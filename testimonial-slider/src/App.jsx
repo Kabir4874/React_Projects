0f0c52927b3d2333391fc8d549ca8095013d8fbc
Kabir4874 <kabir.cse.bd@gmail.com>
2024-02-29 13:57:03 +0600

    add

--- testimonial-slider/src/App.jsx
+++ testimonial-slider/src/App.jsx
@@ -1,18 +1,16 @@
-import Testimonial from "./components/Testimonial"
-import reviews from './Data'
+import Testimonial from "./components/Testimonial";
+import reviews from "./Data";
 
 function App() {
-
-
   return (
-    <div>
-      <div>
-        <h1>Our Testimonials</h1>
-        <div></div>
-        <Testimonial reviews={reviews}/>
+    <div className=" flex flex-col w-[100vw] h-[100vh] justify-center items-center bg-gray-200">
+      <div className=" text-center">
+        <h1 className=" text-4xl font-bold">Our Testimonials</h1>
+        <div className=" bg-violet-400 h-[4px] w-[1/5] mt-1"></div>
+        <Testimonial reviews={reviews} />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
