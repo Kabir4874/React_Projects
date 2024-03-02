9c68d1c9d67fc6943b800bcc8045df95fa08b0fa
Kabir4874 <kabir.cse.bd@gmail.com>
2024-03-02 20:46:38 +0600

    add

--- study-notion/src/App.jsx
+++ study-notion/src/App.jsx
@@ -1,11 +1,24 @@
-
-
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import { useState } from "react";
 function App() {
-  
-
+  const [isLoggedIn, setIsLoggedIn]= useState(false);
   return (
-   <div>App</div>
-  )
+    <div>
+      <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
