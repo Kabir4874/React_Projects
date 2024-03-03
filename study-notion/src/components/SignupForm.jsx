b333dd0fc5e4dade4ab5e22f8392aa7aa3fba4ae
Kabir4874 <kabir.cse.bd@gmail.com>
2024-03-03 19:54:54 +0600

    add

--- study-notion/src/components/SignupForm.jsx
+++ study-notion/src/components/SignupForm.jsx
@@ -47,7 +47,7 @@ const SignupForm = ({ setIsLoggedIn }) => {
         <button
           onClick={() => setAccountType("instructor")}
           className={`${
-            accountType === "student"
+            accountType === "instructor"
               ? " bg-richblack-900 text-richblack-5"
               : " bg-transparent text-richblack-200"
           } py-2 px-5 rounded-full transition-all duration-200`}
