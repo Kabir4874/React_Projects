01b6882b06538c6df7371625086b302ffc8e75d5
Kabir4874 <kabir.cse.bd@gmail.com>
2024-03-06 18:36:18 +0600

    add

---
+++ random-gifs-generator/src/App.jsx
@@ -0,0 +1,19 @@
+import Random from "./components/Random"
+import Tag from "./components/Tag"
+
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1 className="bg-white rounded-sm">RANDOM GIFS</h1>
+      <div className="flex flex-col">
+        <Random/>
+        <Tag/>
+      </div>
+    </div>
+  )
+}
+
+export default App
