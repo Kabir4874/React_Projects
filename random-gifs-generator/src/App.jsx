92303f61e1b1a943bbf47b9d1600e3d4881ccb64
Kabir4874 <kabir.cse.bd@gmail.com>
2024-03-06 18:49:28 +0600

    add

--- random-gifs-generator/src/App.jsx
+++ random-gifs-generator/src/App.jsx
@@ -1,19 +1,18 @@
-import Random from "./components/Random"
-import Tag from "./components/Tag"
-
+import Random from "./components/Random";
+import Tag from "./components/Tag";
 
 function App() {
-  
-
   return (
-    <div>
-      <h1 className="bg-white rounded-sm">RANDOM GIFS</h1>
-      <div className="flex flex-col">
-        <Random/>
-        <Tag/>
+    <div className="w-full h-screen flex flex-col background relative overflow-x-hidden items-center">
+      <h1 className="bg-white rounded-lg w-11/12 text-center mt-[40px] px-10 py-2 text-4xl font-bold">
+        RANDOM GIFS
+      </h1>
+      <div className="flex flex-col w-full items-center gap-y-10 mt-8">
+        <Random />
+        <Tag />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- random-gifs-generator/src/components/Random.jsx
+++ random-gifs-generator/src/components/Random.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Random = () => {
+  const [gif, setGif] = useState("");
+  function clickHandler() {}
   return (
-    <div>Random</div>
-  )
-}
+    <div className=" w-1/2 h-[450px] bg-green-500 rounded-lg border border-black flex flex-col items-center">
+      <h1>Random Gif</h1>
+      <img src={gif} width="450" />
+      <button onClick={clickHandler}>Generate</button>
+    </div>
+  );
+};
 
-export default Random
+export default Random;
