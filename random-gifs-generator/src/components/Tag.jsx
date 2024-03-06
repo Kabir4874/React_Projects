46e4f67f11d34874c8d7202790276543365142e8
Kabir4874 <kabir.cse.bd@gmail.com>
2024-03-06 20:07:28 +0600

    add

--- random-gifs-generator/src/components/Tag.jsx
+++ random-gifs-generator/src/components/Tag.jsx
@@ -11,7 +11,7 @@ const Tag = () => {
   async function fetchData() {
     try {
       setLoading(true);
-      const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+      const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`;
       const { data } = await axios.get(url);
       const imageSource = data.data.images.downsized_large.url;
       setGif(imageSource);
@@ -32,7 +32,7 @@ const Tag = () => {
   return (
     <div className=" w-1/2 bg-blue-400 rounded-lg border border-black flex flex-col items-center gap-y-5 mt-3">
       <h1 className=" mt-4 text-2xl underline uppercase font-bold">
-        Random Gif
+        Random {tag} Gif
       </h1>
       {loading ? <Spinner /> : <img src={gif} width="450" />}
 
