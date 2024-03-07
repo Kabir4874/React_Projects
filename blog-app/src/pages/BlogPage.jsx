20fb07f47a00a02cefb2635152ba17c4291b9b26
Kabir4874 <kabir.cse.bd@gmail.com>
2024-03-07 19:01:19 +0600

    add

--- blog-app/src/pages/BlogPage.jsx
+++ blog-app/src/pages/BlogPage.jsx
@@ -1,6 +1,9 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useLocation, useNavigation } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
+import Header from "../components/Header";
+import Spinner from "../components/Spinner";
+import BlogDetails from "../components/BlogDetails";
 
 const BlogPage = () => {
   const [blog, setBlog] = useState(null);
@@ -15,14 +18,44 @@ const BlogPage = () => {
     let url = `${baseUrl}?blogId=${blogId}`;
     try {
       const res = await fetch(url);
-      const data= res.json();
+      const data = res.json();
       setBlog(data.blog);
       setRelatedBlogs(data.relatedBlogs);
     } catch (error) {
       console.log(error);
+      setBlog(null);
+      setRelatedBlogs([]);
     }
+    setLoading(false);
   }
-  return <div>Hello</div>;
+  useEffect(() => {
+    if (blogId) {
+      fetchRelatedBlogs();
+    }
+  }, [location.pathname]);
+  return (
+    <div>
+      <Header />
+      <div>
+        <button onClick={() => navigation(-1)}>Back</button>
+      </div>
+      {loading ? (
+        <Spinner />
+      ) : blog ? (
+        <div>
+          <BlogDetails post={blog} />
+          <h2>Related Blogs</h2>
+          {relatedBlogs.map((post) => (
+            <div key={post.id}>
+              <BlogDetails post={post} />
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div> No Blog Found</div>
+      )}
+    </div>
+  );
 };
 
 export default BlogPage;
