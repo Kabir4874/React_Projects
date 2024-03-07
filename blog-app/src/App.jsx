81a921079b4441ba02d4affa8d42d2c14aec03d6
Kabir4874 <kabir.cse.bd@gmail.com>
2024-03-07 14:00:40 +0600

    add

--- blog-app/src/App.jsx
+++ blog-app/src/App.jsx
@@ -1,8 +1,14 @@
+import { useContext, useEffect } from "react";
 import Blogs from "./components/Blogs";
 import Header from "./components/Header";
 import Pagination from "./components/Pagination";
+import { AppContext } from "./context/AppContext";
 
 function App() {
+  const { fetchBlogPosts } = useContext(AppContext);
+  useEffect(() => {
+    fetchBlogPosts();
+  }, []);
   return (
     <div>
       <Header />
--- blog-app/src/components/Header.jsx
+++ blog-app/src/components/Header.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div>
+      <header>
+        <h1>CodeHelp Blogs</h1>
+      </header>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
