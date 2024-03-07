052e805b41875ad035dd6b49ee538ce214856707
Kabir4874 <kabir.cse.bd@gmail.com>
2024-03-07 14:12:24 +0600

    add

--- blog-app/src/components/Pagination.jsx
+++ blog-app/src/components/Pagination.jsx
@@ -1,9 +1,20 @@
-
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
 const Pagination = () => {
+  const { page, handlePageChange, totalPages } = useContext(AppContext);
   return (
-    <div>Pagination</div>
-  )
-}
+    <div>
+      <div>
+        {page > 1 && (
+          <button onClick={() => handlePageChange(page - 1)}>Previous</button>
+        )}
+        {page < totalPages && (
+          <button onClick={() => handlePageChange(page + 1)}>Next</button>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default Pagination
+export default Pagination;
