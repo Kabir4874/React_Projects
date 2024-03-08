727667e866adb3d67e6b8a2f3dd6dc86761b6a93
Kabir4874 <kabir.cse.bd@gmail.com>
2024-03-08 12:34:26 +0600

    add

--- shopping-cart-app/src/App.jsx
+++ shopping-cart-app/src/App.jsx
@@ -1,13 +1,20 @@
-
+import { Route, Routes } from "react-router";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
 
 function App() {
-  
-
   return (
-   <div>
-    Hello
-   </div>
-  )
+    <div>
+      <div>
+        <Navbar />
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ shopping-cart-app/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const API_URL= 'https://fakestoreapi.com/products';
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
