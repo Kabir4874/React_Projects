7ddff404018beb02844ba98570a489fdaf8e9274
Kabir4874 <kabir.cse.bd@gmail.com>
2024-03-08 16:09:02 +0600

    add

--- shopping-cart-app/src/components/Navbar.jsx
+++ shopping-cart-app/src/components/Navbar.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { FaShoppingCart } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div>
+      <div className=" flex justify-between">
+        <NavLink to="/">
+          <div>
+            <img src="../assets/logo.png" />
+          </div>
+        </NavLink>
+        <div>
+          <NavLink to="/">
+            <p>Home</p>
+          </NavLink>
+          <NavLink to="/cart">
+            <div>
+              <FaShoppingCart />
+            </div>
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- shopping-cart-app/src/pages/Home.jsx
+++ shopping-cart-app/src/pages/Home.jsx
@@ -1,11 +1,40 @@
-
-
-const API_URL= 'https://fakestoreapi.com/products';
+import { useEffect, useState } from "react";
+import Spinner from "../components/Spinner";
+import Product from "../components/Product";
 
 const Home = () => {
+  const API_URL = "https://fakestoreapi.com/products";
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState([]);
+  async function fetchProductData() {
+    setLoading(true);
+    try {
+      const res = await fetch(API_URL);
+      const data = res.json();
+      setPosts(data);
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  }
+  useEffect(() => {
+    fetchProductData();
+  }, []);
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      {loading ? (
+        <Spinner />
+      ) : posts.length > 0 ? (
+        <div>
+          {posts.map((post) => (
+            <Product key={post.id} post={post} />
+          ))}
+        </div>
+      ) : (
+        <p>No Data Found</p>
+      )}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
