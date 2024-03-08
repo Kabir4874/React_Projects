596ba6980375fa94d63bdafb43b7a2f1199b4ae1
Kabir4874 <kabir.cse.bd@gmail.com>
2024-03-08 21:03:40 +0600

    add

--- movix/src/App.jsx
+++ movix/src/App.jsx
@@ -1,8 +1,15 @@
-
+import { useEffect } from "react";
+import { fetchDataFromApi } from "./utils/api.js";
 function App() {
-  return (
-   <div className='App'>App</div>
-  )
+  useEffect(() => {
+    apiTesting();
+  }, []);
+  const apiTesting = () => {
+    fetchDataFromApi("/movie/popular").then((res) => {
+      console.log(res);
+    });
+  };
+  return <div className="App">App</div>;
 }
 
-export default App
+export default App;
