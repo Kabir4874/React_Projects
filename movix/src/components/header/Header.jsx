34fe8683e5254eccec6488d3df28cf9dc89ccc2b
Kabir4874 <kabir.cse.bd@gmail.com>
2024-03-10 19:25:51 +0600

    add

--- movix/src/components/header/Header.jsx
+++ movix/src/components/header/Header.jsx
@@ -1,7 +1,21 @@
 
+import { useState } from 'react'
 import './Header.scss'
+import { useLocation, useNavigate } from 'react-router-dom';
+import logo from '../../assets/movix-logo.svg';
+import { HiOutlineSearch } from "react-icons/hi";
+import { SlMenu } from "react-icons/sl";
+import { VscChromeClose } from "react-icons/vsc";
+import ContentWrapper from '../contentWrapper/ContentWrapper';
 
 const Header = () => {
+  const [show,setShow]= useState('top');
+  const [lastScrollY,setLastScrollY]= useState(0);
+  const [mobileMenu, setMobileMenu]= useState(false);
+  const [query,setQuery]=useState('');
+  const [showSearch, setShowSearch]= useState('');
+  const navigate= useNavigate();
+  const location= useLocation();
   return (
     <div>Header</div>
   )
