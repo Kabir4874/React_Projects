1ea5fcb04c77d75ed1fad2457cac57da46f9a7a6
Kabir4874 <kabir.cse.bd@gmail.com>
2024-03-12 19:00:46 +0600

    add

--- movix/src/pages/details/Details.jsx
+++ movix/src/pages/details/Details.jsx
@@ -1,9 +1,6 @@
-
-import './Details.scss'
+import "./Details.scss";
 const Details = () => {
-  return (
-    <div>Details</div>
-  )
-}
+  return <div>Details</div>;
+};
 
-export default Details
+export default Details;
