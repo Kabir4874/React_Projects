4d631df470eb23f6d12346a2e3f1afa538611cf0
Kabir4874 <kabir.cse.bd@gmail.com>
2024-03-14 19:14:08 +0600

    add

--- youtube-clone/src/components/VideoCard.jsx
+++ youtube-clone/src/components/VideoCard.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { BsFillCheckCircleFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import VideoLength from "../shared/VideoLength";
 
-const VideoCard = () => {
+const VideoCard = ({ video }) => {
   return (
-    <div>VideoCard</div>
-  )
-}
+    <Link to={`/video/${video.videoId}`}>
+      <div className=" flex flex-col mb-8">
+        <div className="relative h-48 md:h-40 md:rounded-xl overflow-hidden">
+          <img
+            src={video?.thumbnails?.[0]?.url}
+            className=" h-full w-full object-cover"
+          />
+          {video?.lengthSeconds && <VideoLength time={video?.lengthSeconds} />}
+        </div>
+        <div className=" flex text-white mt-3">
+          <div className=" flex items-start">
+            <div className=" flex h-9 w-9 rounded-full overflow-hidden">
+              <img
+                className=" h-full w-full object-cover"
+                src={video?.author?.avatar[0]?.url}
+              />
+            </div>
+          </div>
+          <div className=" flex flex-col ml-3 overflow-hidden">
+            <span className=" text-sm font-bold line-clamp-2">
+              {video?.title}
+            </span>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
 
-export default VideoCard
+export default VideoCard;
