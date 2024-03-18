b621c8d69041508add5d0bee4e5cf30865581e4c
Kabir4874 <kabir.cse.bd@gmail.com>
2024-03-18 22:06:10 +0600

    add

--- filmy-verse/src/components/Cards.jsx
+++ filmy-verse/src/components/Cards.jsx
@@ -1,9 +1,17 @@
-
-
 const Cards = () => {
   return (
-    <div>Cards</div>
-  )
-}
+    <div className=" flex flex-wrap justify-between p-3 mt-2">
+      <div className=" bg-gray-900 shadow-lg p-2 hover:-translate-y-2 transition-all duration-300 cursor-pointer rounded-md">
+        <img
+          src="https://www.komar.de/media/catalog/product/cache/5/image/9df78eab33525d08d6e5fb8d27136e95/v/d/vd-046-star-wars-official-poster-ep7.jpg"
+          className=" h-80 rounded-md"
+        />
+        <h1>Name: Star Wars</h1>
+        <h1>Rating: 5</h1>
+        <h1>Year: 2019</h1>
+      </div>
+    </div>
+  );
+};
 
-export default Cards
+export default Cards;
