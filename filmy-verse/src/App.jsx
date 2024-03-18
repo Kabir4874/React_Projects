845757232be50eb67883732b61c348928211f40c
Kabir4874 <kabir.cse.bd@gmail.com>
2024-03-18 21:57:00 +0600

    add

--- filmy-verse/src/App.jsx
+++ filmy-verse/src/App.jsx
@@ -1,13 +1,13 @@
-import Header from "./components/Header"
+import Cards from "./components/Cards";
+import Header from "./components/Header";
 
 function App() {
-  
-
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <Cards />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- filmy-verse/src/components/Header.jsx
+++ filmy-verse/src/components/Header.jsx
@@ -1,14 +1,17 @@
 import AddIcon from "@mui/icons-material/Add";
+import { Button } from "@mui/material";
 const Header = () => {
   return (
-    <div className=" text-3xl text-red-500 font-bold p- border-b-2 border-gray-500 flex justify-between">
-      <span>
+    <div className=" text-3xl text-red-500 font-bold p-3 border-b-2 border-gray-500 flex justify-between">
+      <span className=" cursor-pointer">
         Filmy<span className=" text-white">Verse</span>
       </span>
-      <h1 className="">
-        <AddIcon className=" mr-2" />
-        Add New
-      </h1>
+      <Button>
+        <h1 className=" text-lg text-white flex items-center cursor-pointer">
+          <AddIcon className=" mr-1" color="secondary" />
+          Add New
+        </h1>
+      </Button>
     </div>
   );
 };
