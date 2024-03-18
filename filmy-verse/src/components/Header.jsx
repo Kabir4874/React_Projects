b3f67fe99b911c76b287831247b2854866a7e0f0
Kabir4874 <kabir.cse.bd@gmail.com>
2024-03-18 21:48:30 +0600

    add

--- filmy-verse/src/components/Header.jsx
+++ filmy-verse/src/components/Header.jsx
@@ -1,9 +1,16 @@
-
-
+import AddIcon from "@mui/icons-material/Add";
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className=" text-3xl text-red-500 font-bold p- border-b-2 border-gray-500 flex justify-between">
+      <span>
+        Filmy<span className=" text-white">Verse</span>
+      </span>
+      <h1 className="">
+        <AddIcon className=" mr-2" />
+        Add New
+      </h1>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
