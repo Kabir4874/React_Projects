ea9522a510889b7a55f0cfb91c611f0504361c9b
Kabir4874 <kabir.cse.bd@gmail.com>
2024-03-18 23:01:06 +0600

    add

--- filmy-verse/src/components/Cards.jsx
+++ filmy-verse/src/components/Cards.jsx
@@ -1,15 +1,35 @@
+import { useState } from "react";
+
 const Cards = () => {
+  const [data, setData] = useState([
+    {
+      name: "Star Wars",
+      year: "2019",
+      rating: 5,
+      img: "https://www.komar.de/media/catalog/product/cache/5/image/9df78eab33525d08d6e5fb8d27136e95/v/d/vd-046-star-wars-official-poster-ep7.jpg",
+    },
+  ]);
   return (
     <div className=" flex flex-wrap justify-between p-3 mt-2">
-      <div className=" bg-gray-900 shadow-lg p-2 hover:-translate-y-2 transition-all duration-300 cursor-pointer rounded-md">
-        <img
-          src="https://www.komar.de/media/catalog/product/cache/5/image/9df78eab33525d08d6e5fb8d27136e95/v/d/vd-046-star-wars-official-poster-ep7.jpg"
-          className=" h-80 rounded-md"
-        />
-        <h1>Name: Star Wars</h1>
-        <h1>Rating: 5</h1>
-        <h1>Year: 2019</h1>
-      </div>
+      {data.map((element, index) => {
+        return (
+          <div
+            key={index}
+            className=" bg-gray-900 shadow-lg p-2 hover:-translate-y-3 transition-all duration-500 cursor-pointer rounded-md font-medium mt-6"
+          >
+            <img src={element.img} className=" h-80 rounded-md" />
+            <h1>
+              <span className=" text-gray-500">Name:</span> {element.name}
+            </h1>
+            <h1>
+              <span className=" text-gray-500">Rating:</span> {element.rating}
+            </h1>
+            <h1>
+              <span className=" text-gray-500">Year:</span> {element.year}
+            </h1>
+          </div>
+        );
+      })}
     </div>
   );
 };
