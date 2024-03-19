6893e9a9be523052b130c60a7b75ba9da2d5ae5a
Kabir4874 <kabir.cse.bd@gmail.com>
2024-03-19 15:43:18 +0600

    add

--- shopping-cart-app/src/components/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-const Spinner = () => {
-  return (
-    <div className=" w-screen h-screen flex items-center justify-center">
-      <div className="spinner"></div>
-    </div>
-  );
-};
-
-export default Spinner;
