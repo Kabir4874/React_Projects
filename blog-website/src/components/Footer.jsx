a0eb17227dc97c5fb5e206b85341abf3e9c25c90
Kabir4874 <kabir.cse.bd@gmail.com>
2024-03-21 19:58:26 +0600

    add

--- blog-website/src/components/Footer.jsx
+++ blog-website/src/components/Footer.jsx
@@ -1,8 +1,11 @@
+import { FaFacebook, FaInstagram } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa6";
+
 const Footer = () => {
   return (
     <div className="bg-gray-900">
       <div className=" px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-4">
-        <div>
+        <div className=" grid mb-8 lg:grid-cols-6">
           <div className=" grid grid-cols-2 gap-5 lg:col-span-4 md:grid-cols-4">
             {/* category 1 */}
             <div>
@@ -194,48 +197,51 @@ const Footer = () => {
             <p className=" font-medium tracking-wide text-gray-300">
               Subscribe for updates
             </p>
-            <ul className=" mt-2 space-y-2">
-              <li>
-                <a
-                  href="/"
-                  className=" text-gray-500 transition-colors duration-300 hover:text-orange-500"
-                >
-                  Infopreneur
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  className=" text-gray-500 transition-colors duration-300 hover:text-orange-500"
-                >
-                  Personal
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  className=" text-gray-500 transition-colors duration-300 hover:text-orange-500"
-                >
-                  Wiki
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  className=" text-gray-500 transition-colors duration-300 hover:text-orange-500"
-                >
-                  Forum
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  className=" text-gray-500 transition-colors duration-300 hover:text-orange-500"
-                >
-                  Projects
-                </a>
-              </li>
-            </ul>
+            <form className=" mt-4 flex flex-col md:flex-row">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="flex-grow w-full h-12 px-4 mb-3 transition duration-300 bg-white border border-gray-300 rounded shadow-sm aspect-auto md:mr-2 md:mb-0 focus:border-purple-400 focus:outline-none"
+              />
+
+              <button
+                type="submit"
+                className=" inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-300 rounded shadow-md hover:bg-orange-500 focus:outline-none border"
+              >
+                Subscribe
+              </button>
+            </form>
+            <p className=" mt-4 text-gray-500 text-sm">
+              I Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+              nisi cumque voluptas. Fuga optio aut saepe voluptas adipisci
+              temporibus ducimus omnis explicabo consequuntur! ld dolores
+              corrupti nemo? Ducimus, quidem
+            </p>
+          </div>
+        </div>
+
+        <div className=" flex flex-col justify-between pt-5 pb-10 border-t border-gray-800 sm:flex-row">
+          <p className="text-sm text-gray-500">Copyright 2024 | Made by Kabir Ahmed Ridoy</p>
+          <div className="flex items-center mt-4 space-x-4 sm:mt-0">
+            <a
+              href="#"
+              className=" text-gray-500 transition-all duration-300 hover:text-orange-500"
+            >
+              <FaTwitter />
+            </a>
+            <a
+              href="#"
+              className=" text-gray-500 transition-all duration-300 hover:text-orange-500"
+            >
+              <FaInstagram />
+            </a>
+            <a
+              href="#"
+              className=" text-gray-500 transition-all duration-300 hover:text-orange-500"
+            >
+              <FaFacebook />
+            </a>
           </div>
         </div>
       </div>
