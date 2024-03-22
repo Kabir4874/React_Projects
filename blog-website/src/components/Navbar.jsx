94ab1791dbb3456cf35497547dd8f375dfae1865
Kabir4874 <kabir.cse.bd@gmail.com>
2024-03-22 10:20:14 +0600

    add

--- blog-website/src/components/Navbar.jsx
+++ blog-website/src/components/Navbar.jsx
@@ -3,9 +3,13 @@ import { FaXmark } from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
 
 import { useState } from "react";
+import Modal from "./Modal";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
@@ -17,6 +21,14 @@ const Navbar = () => {
     { path: "/blogs", link: "Blogs" },
     { path: "/contact", link: "Contact" },
   ];
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
   return (
     <header className=" bg-black text-white fixed top-0 left-0 right-0">
       <nav className=" px-4 py-4 max-w-7xl mx-auto flex justify-between items-center">
@@ -42,10 +54,16 @@ const Navbar = () => {
           <a href="/" className=" hover:text-orange-500">
             <FaTwitter />
           </a>
-          <button className=" bg-orange-500 px-6 py-2 font-medium rounded hover:bg-white hover:text-orange-500 transition-all duration-300 ease-in">
+          <button
+            onClick={openModal}
+            className=" bg-orange-500 px-6 py-2 font-medium rounded hover:bg-white hover:text-orange-500 transition-all duration-300 ease-in"
+          >
             Log in
           </button>
         </div>
+
+        <Modal isOpen={isModalOpen} onClose={closeModal} />
+
         {/* Mobile Menu  */}
         <div className=" md:hidden">
           <button className=" cursor-pointer" onClick={toggleMenu}>
