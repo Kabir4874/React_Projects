84599df5c10cf204a901e6f38a97c3e7c42bcc5a
Kabir4874 <kabir.cse.bd@gmail.com>
2024-03-24 11:02:57 +0600

    add

--- virtual-landing-page/src/App.jsx
+++ virtual-landing-page/src/App.jsx
@@ -1,12 +1,17 @@
-import Navbar from "./components/Navbar"
-
+import FeatureSection from "./components/FeatureSection";
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <>
-      <Navbar/>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
