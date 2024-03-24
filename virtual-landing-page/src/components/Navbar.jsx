97d51e96f9b20a36006849ab55cb59f7682bffea
Kabir4874 <kabir.cse.bd@gmail.com>
2024-03-24 10:32:45 +0600

    add

--- virtual-landing-page/src/components/Navbar.jsx
+++ virtual-landing-page/src/components/Navbar.jsx
@@ -35,9 +35,33 @@ const Navbar = () => {
             </a>
           </div>
           <div className="lg:hidden md:flex flex-col justify-end">
-            <button></button>
+            <button onClick={toggleNavbar}>
+              {mobileDrawerOpen ? <X /> : <Menu />}
+            </button>
           </div>
         </div>
+        {mobileDrawerOpen && (
+          <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden">
+            <ul>
+              {navItems.map((item, index) => (
+                <li key={index} className="py-4">
+                  <a href={item.href}>{item.label}</a>
+                </li>
+              ))}
+            </ul>
+            <div className="flex space-x-6">
+              <a href="#" className="py-2 px-3 border rounded-md">
+                Sign In
+              </a>
+              <a
+                href="#"
+                className="py-2 px-3 rounded-md bg-gradient-to-r from-orange-500 to-orange-800"
+              >
+                Create An Account
+              </a>
+            </div>
+          </div>
+        )}
       </div>
     </nav>
   );
