ffc604850913b20b2322a6a2b33f8d2b3c48fbff
Kabir4874 <kabir.cse.bd@gmail.com>
2024-03-26 15:20:39 +0600

    add

--- hoobank/src/components/Footer.jsx
+++ hoobank/src/components/Footer.jsx
@@ -41,13 +41,18 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className=" w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t border-t-[#3f3r45]">
+      <div className=" w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t border-t-[#3f3e45]">
         <p className=" font-poppins font-normal text-[18px] leading-[27px] text-white text-center">
           2024 HooBank || Made By Kabir Ahmed Ridoy
         </p>
         <div className="flex flex-row md:mt-0 mt-6 space-x-6">
           {socialMedia.map((social) => (
-            <img src={social.icon} alt={social.id} key={social.id} className={`w-[21px] h-[21px] object-contain cursor-pointer`}/>
+            <img
+              src={social.icon}
+              alt={social.id}
+              key={social.id}
+              className={`w-[21px] h-[21px] object-contain cursor-pointer`}
+            />
           ))}
         </div>
       </div>
