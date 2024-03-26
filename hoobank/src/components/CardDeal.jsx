35b7559f5ff4e24a2a95d4d991fc4ff0a78c2216
Kabir4874 <kabir.cse.bd@gmail.com>
2024-03-26 13:38:14 +0600

    add

--- hoobank/src/components/CardDeal.jsx
+++ hoobank/src/components/CardDeal.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
 const CardDeal = () => {
   return (
-    <div>CardDeal</div>
-  )
-}
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Find a better card deal <br className=" sm:block hidden" /> in few eay
+          steps.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni
+          doloribus ipsum omnis tempore maxime illum?
+        </p>
+        <Button styles="mt-10" />
+      </div>
 
-export default CardDeal
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card" className=" w-full h-full" />
+      </div>
+    </section>
+  );
+};
+
+export default CardDeal;
