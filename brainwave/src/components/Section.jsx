900e293d267acf1ef12583cad0ada3832756f548
Kabir4874 <kabir.cse.bd@gmail.com>
2024-03-29 15:43:45 +0600

    add

--- brainwave/src/components/Section.jsx
+++ brainwave/src/components/Section.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const Section = ({
-    className,
-    id,
-    coresses
+  className,
+  id,
+  crosses,
+  crossesOffset,
+  customPaddings,
+  children,
 }) => {
   return (
-    <div>Section</div>
-  )
-}
+    <div
+      className={`relative ${
+        customPaddings ||
+        `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""} ${
+          className || ""
+        }`
+      }`}
+    >
+      {children}
 
-export default Section
+      <div className="hidden"></div>
+    </div>
+  );
+};
+
+export default Section;
