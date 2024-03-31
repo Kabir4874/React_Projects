6808a905c58a2f2b168c1e80bafd8e233f238a49
Kabir4874 <kabir.cse.bd@gmail.com>
2024-03-31 20:50:16 +0600

    add

--- brainwave/src/components/Button.jsx
+++ brainwave/src/components/Button.jsx
@@ -12,7 +12,6 @@ const Button = ({ className, href, onClick, children, px, white }) => {
       {ButtonSvg(white)}
     </button>
   );
-
   const renderLink = () => (
     <a href={href} className={classes}>
       <span className={spanClasses}>{children}</span>
