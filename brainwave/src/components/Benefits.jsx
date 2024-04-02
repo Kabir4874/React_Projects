28ea4002323b78c1145e922208ddb1880d153e5d
Kabir4874 <kabir.cse.bd@gmail.com>
2024-04-02 20:19:03 +0600

    add

--- brainwave/src/components/Benefits.jsx
+++ brainwave/src/components/Benefits.jsx
@@ -2,6 +2,8 @@ import { benefits } from "../constants";
 import Heading from "./Heading";
 import Section from "./Section";
 import Arrow from "../assets/svg/Arrow";
+import { GradientLight } from "./design/Benefits";
+import ClipPath from "../assets/svg/ClipPath";
 
 const Benefits = () => {
   return (
@@ -30,10 +32,30 @@ const Benefits = () => {
                     width={48}
                     height={48}
                   />
-                  <p>Explore More</p>
+                  <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
+                    Explore More
+                  </p>
                   <Arrow />
                 </div>
               </div>
+              {item.light && <GradientLight />}
+              <div
+                className="absolute inset-0.5 bg-n-8"
+                style={{ clipPath: "url(#benefits)" }}
+              >
+                <div className=" absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
+                  {item.imageUrl && (
+                    <img
+                      src={item.imageUrl}
+                      alt={item.title}
+                      width={380}
+                      height={362}
+                      className="w-full h-full object-cover"
+                    />
+                  )}
+                </div>
+              </div>
+              <ClipPath />
             </div>
           ))}
         </div>
