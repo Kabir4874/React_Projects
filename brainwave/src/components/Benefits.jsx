274d56ff204ce37ae7a99fa5fe682a4c1e56767d
Kabir4874 <kabir.cse.bd@gmail.com>
2024-04-02 19:44:09 +0600

    add

--- brainwave/src/components/Benefits.jsx
+++ brainwave/src/components/Benefits.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import Heading from "./Heading";
+import Section from "./Section";
 
 const Benefits = () => {
   return (
-    <div>Benefits</div>
-  )
-}
+    <Section id="features">
+      <div className="container relative z-2">
+        <Heading
+          className="md:max-w-md lg:max-w-2xl"
+          title="Chat Smarter, Not Harder with Brainwave"
+        />
+      </div>
+    </Section>
+  );
+};
 
-export default Benefits
+export default Benefits;
