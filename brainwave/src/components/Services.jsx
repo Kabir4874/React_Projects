de17504deb0aa882d12cf0f08564dd63f3acf6f6
Kabir4874 <kabir.cse.bd@gmail.com>
2024-04-03 16:39:56 +0600

    add

--- brainwave/src/components/Services.jsx
+++ brainwave/src/components/Services.jsx
@@ -1,4 +1,6 @@
-import { service1 } from "../assets";
+import { check, service1 } from "../assets";
+import { brainwaveServices } from "../constants";
+import Generating from "./Generating";
 import Heading from "./Heading";
 import Section from "./Section";
 
@@ -20,6 +22,30 @@ const Services = () => {
                 width={800}
                 height={730}
               />
+            </div>
+            <div className="relative z-1 max-w-[17rem] ml-auto">
+              <h4 className="h4 mb-4">Smartest AI</h4>
+              <p className="body-2 mb-[3rem] text-n-3">
+                Brainwave unlocks the potential of AI-powered application
+              </p>
+              <ul className="body-2">
+                {brainwaveServices.map((item, index) => (
+                  <li
+                    key={index}
+                    className="flex items-start py-4 border-t border-n-6"
+                  >
+                    <img src={check} alt="" width={24} height={24} />
+                    <p className="ml-4">{item}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <Generating className="absolute left-4 right-4 bottom-4 border-n-1/10 border lg:left-1/2 lg:right-auto lg:bottom-8 lg:-translate-x-1/2" />
+          </div>
+
+          <div className="relative z-1 grid gap-5 lg:grid-cols-2">
+            <div className="relative min-h-[39rem] border border-n-1/10 rounded-3xl overflow-hidden">
+
             </div>
           </div>
         </div>
